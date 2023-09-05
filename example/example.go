4e9447217ce2e0375735ac3f1cbc5453d2d2117c
surattikorn <surattikorn@gmail.com>
2023-09-05 16:02:22 +0700

    feat: add test with example data

--- example/example.go
+++ example/example.go
@@ -1,29 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
 	"github.com/golfz/gopdfgen"
+	"html/template"
 	"log"
+	"strconv"
+	"time"
 )
 
-var websiteURL = "https://iamgolfz.com"
+const outputFilePath = "test-url.pdf"
 
-var outputFilePath = "test-url.pdf"
+const testPassword = "1q2w3e4r"
 
-var headerFilePath = "html/header.html"
-var footerFilePath = "html/footer.html"
-
-var testPassword = "1q2w3e4r"
-
-//go:embed html/*
+//go:embed webserver/html/*
 var htmlFS embed.FS
 
+//go:embed webserver/templates/*
+var templateFS embed.FS
+
+const bodyTemplateFilePath = "webserver/templates/body.go.html"
+const headerFilePath = "webserver/html/header.html"
+const footerFilePath = "webserver/html/footer.html"
+
+const bodyURL = "http://localhost:18080/body"
+const headerURL = "http://localhost:18080/header"
+const footerURL = "http://localhost:18080/footer"
+
 func main() {
-	testWithURL()
-	//testWithHTML()
+	//testWithURL()
+	testWithHTML()
 }
 
+// testWithURL generates pdf with url
+// please run webserver/webserver.go first
 func testWithURL() {
 	pdfg, err := gopdfgen.NewPDFGenerator()
 	if err != nil {
@@ -35,21 +47,13 @@ func testWithURL() {
 	//pdfg.SetTempDir("_new_gopdfgen_temp")
 
 	// set body url
-	pdfg.SetBodyURL(websiteURL)
+	pdfg.SetBodyURL(bodyURL)
 
-	headerHTML, err := htmlFS.ReadFile(headerFilePath)
-	if err != nil {
-		log.Println(err)
-	}
+	// set header and footer url
+	pdfg.SetHeaderURL(headerURL)
+	pdfg.SetFooterURL(footerURL)
 
-	footerHTML, err := htmlFS.ReadFile(footerFilePath)
-	if err != nil {
-		log.Println(err)
-	}
-
-	pdfg.SetHeaderHTML(headerHTML)
-	pdfg.SetFooterHTML(footerHTML)
-
+	// set password
 	pdfg.SetPassword(testPassword)
 
 	// generate pdf to internal buffer
@@ -70,37 +74,96 @@ func testWithURL() {
 	fmt.Printf("Done, %d bytes", len(b))
 }
 
-//func testWithHTML() {
-//	pdfg, err := NewPDFGenerator()
-//	if err != nil {
-//		log.Panic(err)
-//	}
-//	defer pdfg.Cleanup()
-//
-//	// not necessary because it's default, but you can set the specific path
-//	pdfg.TempDir.Set("_gopdfgen_temp")
-//
-//	// set body url
-//	pdfg.BodyHTML.Set("<html><body><h1>Hello world</h1></body></html>")
-//
-//	// set header and footer url
-//	pdfg.HeaderHTML.Set("<html><body><h1>Hello world</h1></body></html>")
-//	pdfg.FooterHTML.Set("<html><body><h1>Hello world</h1></body></html>")
-//
-//	// generate pdf to internal buffer
-//	err = pdfg.Generate()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// write pdf to file
-//	err = pdfg.WriteFile("./simplesample.pdf")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// get pdf as bytes
-//	b := pdfg.Bytes()
-//
-//	fmt.Sprintf("Done, %d bytes", len(b))
-//}
+func testWithHTML() {
+	pdfg, err := gopdfgen.NewPDFGenerator()
+	if err != nil {
+		log.Println(err)
+	}
+	defer pdfg.Cleanup()
+
+	// not necessary because it's default, but you can set the specific path
+	//pdfg.SetTempDir("_new_gopdfgen_temp")
+
+	// set body html
+	bodyHTML := getBodyHTML()
+	pdfg.SetBodyHTML(bodyHTML)
+
+	headerHTML, err := htmlFS.ReadFile(headerFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	footerHTML, err := htmlFS.ReadFile(footerFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// set header and footer html
+	pdfg.SetHeaderHTML(headerHTML)
+	pdfg.SetFooterHTML(footerHTML)
+
+	// set password
+	pdfg.SetPassword(testPassword)
+
+	// generate pdf to internal buffer
+	err = pdfg.Generate()
+	if err != nil {
+		log.Println(err)
+	}
+
+	// write pdf to file
+	err = pdfg.WriteFile(outputFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// get pdf as bytes
+	b := pdfg.Bytes()
+
+	fmt.Printf("Done, %d bytes", len(b))
+}
+
+// ============================================================================================
+
+type BodyData struct {
+	ID   int
+	Name string
+	Time string
+}
+
+func createBodyData() []BodyData {
+	bodyData := make([]BodyData, 0)
+
+	for i := 1; i <= 1000; i++ {
+		bodyData = append(bodyData, BodyData{
+			ID:   i,
+			Name: "Name " + strconv.Itoa(i),
+			Time: time.Now().String(),
+		})
+	}
+
+	return bodyData
+}
+
+func getBodyHTML() []byte {
+	templateStr, err := templateFS.ReadFile(bodyTemplateFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	t, err := template.New("body").Parse(string(templateStr))
+	if err != nil {
+		log.Println(err)
+	}
+
+	bodyData := createBodyData()
+
+	var outbuf bytes.Buffer
+
+	err = t.Execute(&outbuf, bodyData)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return outbuf.Bytes()
+}
--- example/webserver/webserver.go
+++ example/webserver/webserver.go
@@ -1,5 +1,93 @@
 package main
 
-func main() {
+import (
+	"embed"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
 
+//go:embed html/*
+var htmlFS embed.FS
+
+//go:embed templates/*
+var templateFS embed.FS
+
+const (
+	bodyTemplateFilePath = "templates/body.go.html"
+	headerFilePath       = "html/header.html"
+	footerFilePath       = "html/footer.html"
+)
+
+func main() {
+	http.HandleFunc("/body", handlerBody)
+	http.HandleFunc("/header", handlerHeader)
+	http.HandleFunc("/footer", handlerFooter)
+
+	log.Panic(http.ListenAndServe(":18080", nil))
+}
+
+func handlerBody(w http.ResponseWriter, r *http.Request) {
+	templateStr, err := templateFS.ReadFile(bodyTemplateFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	t, err := template.New("body").Parse(string(templateStr))
+	if err != nil {
+		log.Println(err)
+	}
+
+	bodyData := createBodyData()
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	err = t.Execute(w, bodyData)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func handlerHeader(w http.ResponseWriter, r *http.Request) {
+	headerHTML, err := htmlFS.ReadFile(headerFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(headerHTML)
+}
+
+func handlerFooter(w http.ResponseWriter, r *http.Request) {
+	headerHTML, err := htmlFS.ReadFile(footerFilePath)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(headerHTML)
+}
+
+// ============================================================================================
+
+type BodyData struct {
+	ID   int
+	Name string
+	Time string
+}
+
+func createBodyData() []BodyData {
+	bodyData := make([]BodyData, 0)
+
+	for i := 1; i <= 1000; i++ {
+		bodyData = append(bodyData, BodyData{
+			ID:   i,
+			Name: "Name " + strconv.Itoa(i),
+			Time: time.Now().String(),
+		})
+	}
+
+	return bodyData
 }
